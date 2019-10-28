359f475a7599045441f8fd8c43a1d8af81c13b03
PujitYG <48867666+PujitYG@users.noreply.github.com>
2019-10-29 01:52:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// <script type="text/javascript" src="events.json"></script>
+
 function obj(event,rules,rounds, date, time, venue, entry, prize1,prize2) {
         this.event = event;
         this.rules = rules;
@@ -25,9 +25,9 @@ var events=[{
 },
 {
     "event": "BLIND CODING",
-    "rules": "This is a Team Event. Only ‘TWO’ candidates are allowed per team. Questions will be based on C LANGUAGE only as the compiler provided will be GCC C compiler on Ubuntu.Event will consist of two rounds",
+    "rules": "This is a Team Event. Only ‘TWO’ candidates are allowed per team. Questions will be based on C LANGUAGE only as the compiler provided will be GCC C compiler on Ubuntu.Event will consist of two rounds.",
     "rounds": [
-        "In the first round a simple code with syntax errors will be given. Participants have to correct the code and type the code with MONITOR SWITCHED OFF. Ten minutes will be given for correcting the errors and typing the code. The team which will be able to solve it in the given time will be qualified for the next round. Based on the results of first round the teams will be selected for second round",
+        "In the first round a simple code with syntax errors will be given. Participants have to correct the code and type the code with MONITOR SWITCHED OFF. Ten minutes will be given for correcting the errors and typing the code. The team which will be able to solve it in the given time will be qualified for the next round. Based on the results of first round the teams will be selected for second round.",
         "In the second round which is the final round only a problem statement will be given. The Team needs to write a code for the given problem statement by using C Language and type the code in GCC C compiler on Ubuntu with MONITORS SWITCHED OFF. Twenty minutes will be given to each team for completing this task. Winners will be announced based on the results of compilation and execution."
     ],
     "date": "9th",
@@ -40,7 +40,7 @@ var events=[{
 {
     "event": "LOGIX",
     "rules": "This is an individual event consiting of two rounds. Participants will be asked questions based on Aptitude and Logical Reasoning. They need to answer them correctly and quickly within the given time.",
-    "rounds": ["The participants will begiven a set of questions which should be answered in a time span of 20 minutes 5 Participants with most number of correct answers will be selected for second round.",
+    "rounds": ["The participants will be given a set of questions which should be answered in a time span of 20 minutes.5 Participants with most number of correct answers will be selected for second round.",
         "The selected participants will begiven questions and they should be answered in a time span of 10mins.The participant with highest number of points will be declared as winner."
     ],
     "date": "9th",
@@ -51,8 +51,8 @@ var events=[{
     "prize2": "200"
 },
 {
-    "event": "Pair Programming",
-    "rules": "There will be two rounds",
+    "event": "PAIR PROGRAMMING",
+    "rules": "There will be two rounds.",
     "rounds": [
         "The first round is for 30 minutes. The contestants will get their problem statement. Each team gets two problem statements.The solution to the problem can be done in any of the programming language.There should be no comments indicating the problem statement. If found, both of the contestants will be disqualified.After 15  minutes, the contestants will shift places with each other.The team that has both the solutions to the problem statements qualify to the next round.There will be no extra time given to solve.A piece of paper will be given to work out the problem. This sheet must not be exchanged with the other person. If done, it accounts to ground for disqualification.",
         "There will be only one problem statement for each team.The contestants of each team must decide who will go first.Only the first contestant in the team gets to know the problem statement.After 10 minutes, the second contestant is given the chance to figure out the problem and continue the solution.The second contestant has 10 minutes to do so.In the last 2 minutes both the contestants will be given a chance to work on the solution together."
@@ -68,8 +68,8 @@ var events=[{
     "event": "CODEATHON",
     "rules": "This consist of two people. Any Programming Language can be used. This Event consists of two rounds.",
     "rounds": [
-        "Round1: Easy and Intermediate type of problem statements would be given, that should be solved within given time limit. 50% of total participants on the leaderboard after Round 1 would be entering Round 2 for the Final Phase of CodeAthon. In case of tie breakers , total execution time will be considered and promotion to Round 2 will be declared.",
-        "Round2: In the Final Round of CodeAthon, Intermediate and Hard type of problem statements would be given, that should be solved within given time limit. Top 2 on the leaderboard would be declared as winners for CodeAthon. In case of tie breakers, same total execution time will be considered and winners would be declared."
+        "Easy and Intermediate type of problem statements would be given, that should be solved within given time limit. 50% of total participants on the leaderboard after Round 1 would be entering Round 2 for the Final Phase of CodeAthon. In case of tie breakers , total execution time will be considered and promotion to Round 2 will be declared.",
+        "In the Final Round of CodeAthon, Intermediate and Hard type of problem statements would be given, that should be solved within given time limit. Top 2 on the leaderboard would be declared as winners for CodeAthon. In case of tie breakers, same total execution time will be considered and winners would be declared."
     ],
     "date": "8th",
     "time": "2PM",
@@ -80,12 +80,12 @@ var events=[{
 },
 {
     "event": "UI DESIGNING",
-    "rules": "",
+    "rules": "There will be one round. The participants will be given the requirements of the website on spot and they need to design the User Interface for it on Adobe XD. Participants are requested to get their own laptops with Adobe XD installed. The link for installation: https://www.adobe.com/go/dnld-xd .",
     "rounds": [],
     "date": "8th",
     "time": "12PM",
-    "venue": "202",
-    "Entry": "120",
+    "venue": "302",
+    "Entry": "50",
     "prize1": "500",
     "prize2": "200"
 },
@@ -105,7 +105,7 @@ var events=[{
 },
 {
     "event": "FAST TYPING",
-    "rules": "A paragraph will be given to type, the winner will be decided based on who finishes fast with accuracy.The given  paragraph must be typed as it is, keeping in mind: case sensitivity,   punctuations and special characters.Once you finish the given task, inform the coordinator immediately  • The event will be conducted in two rounds.The participants who qualify  first round will move to the  second",
+    "rules": "A paragraph will be given to type, the winner will be decided based on who finishes fast with accuracy.The given  paragraph must be typed as it is, keeping in mind: case sensitivity, punctuations and special characters.Once you finish the given task, inform the coordinator immediately .The event will be conducted in two rounds.The participants who qualify in first round will move to the second round.",
     "rounds": [],
     "date": "9th",
     "time": "11 AM",
@@ -127,7 +127,7 @@ var events=[{
 },
 {
     "event": "PUBG",
-    "rules": "4 player squad ,Battle Royal Erangle map",
+    "rules": "4 player squad,Battle Royal Erangle map.",
     "rounds": [],
     "date": "8th",
     "time": "1PM",
@@ -137,8 +137,8 @@ var events=[{
     "prize2": "1000"
 },
 {
-    "event": "Photography and meme making",
-    "rules": "Certified amount of time will be given to the participants to click a picture and pick a theme for their meme within the college campus. The date when the picture was clicked will be checked prior to the commencement of the time for making the meme. Any mobile application can be used to create the meme and the participant can pick any theme. Stipulated amount of time will be given to all the participants to create the meme. The meme will be judged on basis of creativity. Use of a vulgar theme/language is prohibited. Organizers decision will be final ",
+    "event": "PHOTOGRAPHY AND MEME MAKING",
+    "rules": "Certified amount of time will be given to the participants to click a picture and pick a theme for their meme within the college campus. The date when the picture was clicked will be checked prior to the commencement of the time for making the meme. Any mobile application can be used to create the meme and the participant can pick any theme. Stipulated amount of time will be given to all the participants to create the meme. The meme will be judged on basis of creativity. Use of a vulgar theme/language is prohibited. Organizers decision will be final.",
     "rounds": [],
     "date": "9th",
     "time": "9:30 AM",
@@ -149,7 +149,7 @@ var events=[{
 },
 {
     "event": "COUNTER STRIKE",
-    "rules": "Counter Strike Source. It will be a 2v2 event Will be hosting it on lan in one of the labs.Till finals all the rounds are race to 9 and the finals will be a competitive match for 30 rounds (overtime included) Map pools are Dust 2, Dust, Inferno, and Nuke Knife round decides start side and game starts with pistol round",
+    "rules": "Counter Strike Source. It will be a 2v2 event, will be hosted on lan in one of the labs.Till finals all the rounds are race to 9 and the finals will be a competitive match for 30 rounds (overtime included) Map pools are Dust 2, Dust, Inferno, and Nuke Knife round decides start side and game starts with pistol round.",
     "rounds": [],
     "date": "9th",
     "time": "10AM",
@@ -159,11 +159,11 @@ var events=[{
     "prize2": "400"
 },
 {
-    "event": "Minute to win it",
-    "rules": "1.There will be a total of 4 rounds. All the participants will be given a task to perform. The task has to be completed within 1 min (60 sec) in order to win that round and advance to the next round. Each round has a different challenge. ",
+    "event": "MINUTE TO WIN IT",
+    "rules": "There will be a total of 4 rounds. All the participants will be given a task to perform. The task has to be completed within 1 min (60 sec) in order to win that round and advance to the next round. Each round has a different challenge. ",
     "rounds": [
         "In the first 3 rounds participants have to finish the given task within 60 seconds. Whoever completes the task qualifies to the next round.",
-        "In is the finale round where the person who finishes the task first is the winner"
+        "In is the finale round where the person who finishes the task first is the winner."
     ],
     "date": "8th",
     "time": "12:30 PM",
@@ -174,7 +174,7 @@ var events=[{
 },
 {
     "event": "TV SHOW QUIZ",
-    "rules": "No use of mobile.Two members in a group ",
+    "rules": "No use of mobile.Two members in a group.",
     "rounds": [
         "The participants will be given a set of questions which should be answered in 15 minutes. Participants who passes cut off will be selected for second round.",
         "Participants will be asked 10 questions. If they don’t answer it will pass on to the next team. Based on points winners will be declared."
@@ -188,9 +188,9 @@ var events=[{
 },
 {
     "event": "DIZZY FOOTBALL",
-    "rules": "If caught cheating you will be disqualified. Organizers decision is final. Any such conflicts with the organisers will be disqualified.",
+    "rules": "Organizers decision is final. Any such conflicts with the organisers will be disqualified.",
     "rounds": [
-        "Each player gets three chances to take a shot at the goal, he/she has to spin around the ball(15 spins) kept at a distance from the goal, after 15 rounds shoot the ball towards the goal. If it passes then you are through to the next round .",
+        "Each player gets three chances to take a shot at the goal, he/she has to spin around the ball(15 spins) kept at a distance from the goal, after 15 rounds shoot the ball towards the goal. If it passes then you are through to the next round.",
         "Obstacles will be kept in your way to make the competition more complicated. And the same is continued."
     ],
     "date": "8th",
